cd8303d2d1927c657f8ed3921bb292f3fee0e89c
seregamuravyov <mursmail@gmail.com>
2016-04-04 16:35:17 +0300

    change run script

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-java -classpath target/custom-checker-1.0-jar-with-dependencies.jar com.puppycrawl.tools.checkstyle.Main -c config.xml src/main/java/TestClass.java
+java -classpath target/custom-checker-1.0-jar-with-dependencies.jar com.puppycrawl.tools.checkstyle.Main -c config.xml src/
